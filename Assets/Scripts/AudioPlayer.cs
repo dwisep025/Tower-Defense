cb7ab09ef6cf1934f627bdb654b04a2036b7d5d5
dwisep025 <dwiseptiawan46@gmail.com>
2021-09-16 19:09:09 +0700

    Initial commit

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private List<AudioClip> _audioClips;
+
+    private static AudioPlayer _instance = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static AudioPlayer Instance
+
+    {
+
+        get
+
+        {
+
+            if (_instance == null)
+
+            {
+
+                _instance = FindObjectOfType<AudioPlayer>();
+
+            }
+
+
+
+            return _instance;
+
+        }
+
+    }
+    public void PlaySFX(string name)
+
+    {
+
+        AudioClip sfx = _audioClips.Find(s => s.name == name);
+
+        if (sfx == null)
+
+        {
+
+            return;
+
+        }
+
+
+
+        _audioSource.PlayOneShot(sfx);
+
+    }
+
+}
